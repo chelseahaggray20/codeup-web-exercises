caca6c61e737aeb6da208d79c7705c0467b49f66
Chelseahaggray20 <chelsea.haggray20@gmail.com>
2020-01-07 09:07:35 -0600

    continue of jquery exercise

--- JS/objects.js
+++ JS/objects.js
@@ -11,6 +11,16 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
+    var person = {
+        firstName: "Daniel",
+        lastName: "Fryar",
+        sayHello : function() {
+            return "Hello from " + this.firstName + " " + this.lastName + "!";
+        }
+    }
+
+    console.log(person.firstName);
+    console.log(person.lastName);
 
     /**
      * TODO:
@@ -21,6 +31,10 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
+    // person.sayHello = function() {
+    //     return "Hello from " + this.firstName + " " + this.lastName + "!";
+    // }
+    console.log(person.sayHello());
 
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
@@ -36,11 +50,21 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+    shoppers.forEach(function (shopper) {
+        var discount = 0;
+        if (shopper.amount > 200)
+            discount = shopper.amount * .12;
+        var total = shopper.amount - discount;;''
+        console.log(shopper.name + "'s bill was $" +
+            shopper.amount +
+            ". They received a discount of $" +
+            discount +" and their total was $" +total + ".");
+    })
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
@@ -54,6 +78,202 @@
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
      */
+    var books = [
+        {
+            title: 'Cather in the Rye',
+            author: {
+                firstName: "J.D.",
+                lastName: "Salinger"
+            }
+        },
+        {
+            title: 'The Buffalo Soldier',
+            author: {
+                firstName: "Chris",
+                lastName: "Bohjalian"
+            }
+        },
+        {
+            title: 'Nine Stories',
+            author: {
+                firstName: "J.D.",
+                lastName: "Salinger"
+            }
+        },
+        {
+            title: "The Walking Drum",
+            author: {
+                firstName: "Louis",
+                lastName: "L'Amour"
+            }
+        },
+        {
+            title: "Ready Player One",
+            author: {
+                firstName: "Ernest",
+                lastName: "Cline"
+            }
+        },
+        {
+            title: "Last of the Breed",
+            author: {
+                firstName: "Louis",
+                lastName: "L'Amour"
+            }
+        },
+        {
+            author : {
+                firstName: "Alan",
+                lastName: "Watts"
+            },
+            title : "The Book on The Taboo Against Knowing Who You Are"
+        },
+
+        {
+            author : [
+                {firstName:"Joseph"},
+                {lastName:"Murphy"}
+            ],
+            title : "The Power Of Your Subconscious Mind"
+        },
+
+        {
+            author : [
+                {firstName:"Danny"},
+                {lastName:"Meyer"}
+            ],
+            title : "Setting the Table"
+        },
+        {
+            author : [
+                {firstName:"Ayn"},
+                {lastName:"Rand"}
+            ],
+            title : "Atlas Shrugged"
+        },
+        {
+            title: "The Salmon of Doubt",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "Walkaway",
+            author: {
+                firstName: "Cory",
+                lastName: "Doctorow",
+            }
+        },
+        {
+            title: "A Brief History of Time",
+            author: {
+                firstName: "Stephen",
+                lastName: "Hawking",
+            }
+        },
+        {
+            title: "Moby Dick",
+            author: {
+                firstName: "Herman",
+                lastName: "Melville",
+            }
+        },
+        {
+            title: "Treasure Island",
+            author: {
+                firstName: "Robert",
+                lastName: "Stevenson",
+            }
+        },
+        {author:{firstName: "J.K.", lastName: "Rowling"}, title: "Harry Potter and the Chamber of Secrets"},
+        {author:{firstName: "Arthur.", lastName: "Golden"}, title: "Memoirs of a Geisha"},
+        {author:{firstName: "Dante", lastName: "Alighieri"}, title: "Divine Comedy"},
+        {author:{firstName: "Carl", lastName: "Jung"}, title: "The Archetypes and the Collective Unconscious"},
+        {author:{firstName: "Karl", lastName: "Marx"}, title: "The Communist Manifesto"},
+        {author:{firstName: "A.A.", lastName: "Milne"}, title: "Winnie the Pooh"},
+        {number: 1, title: "Wild At Heart", author:{firstName: "John", lastName: "Eldridge"}},
+        {number: 2, title: "sherlock holmes", author:{firstName: "Author", lastName: "Doyle"}},
+        {number: 3, title: "Daniel Boone", author:{firstName: "John", lastName: "Faragher"}},
+        {number: 4, title: "Linux Bible", author:{firstName: "Christopher", lastName: "Negus"}},
+        {number: 5, title: "How Linux Works", author:{firstName: "Brian", lastName: "Ward"}},
+        {
+            title: "The Rise",
+            author: {
+                firstName: "Nora",
+                lastName: "Roberts"
+            }
+        },
+        {
+            title: "The Guardians",
+            author: {
+                firstName: "John",
+                lastName: "Grisham"
+            }
+        },
+        {
+            title: "A Minute to Midnight",
+            author: {
+                firstName: "John",
+                lastName: "Grisham"
+            }
+        },
+        {
+            title: "Criss Cross",
+            author: {
+                firstName: "David",
+                lastName: "Baldacci"
+            }
+        },
+        {
+            title: "Where the Crawdads Sing",
+            author: {
+                firstName: "Delia",
+                lastName: "Owens"
+            }
+        },
+        {
+            title: "48 Laws of Power",
+            author:{
+                firstName: "Robert",
+                lastName: "Greene",
+            }
+        },
+        {
+            title: "Rich dad, poor dad",
+            author:{
+                firstName: "Robert",
+                lastName: "Kyosaki",
+                report: function() {
+                    console.log(this.firstName + " " + this.lastName + " .");
+                }
+            },
+        },
+        {
+            title: "Greenbacks and Ghetto Politics",
+            author: {
+                firstName: "Paris",
+                lastName: "Tyus",
+                report: function () {
+                    console.log(this.firstName + " " + this.lastName + " .");
+                }
+            },
+        },
+        {
+            title: "Millionaire Mind",
+            author: {
+                firstName: "Thomas",
+                lastName: "Stanley",
+                report: function () {
+                    console.log(this.firstName + " " + this.lastName + " .");
+                }
+            }
+        }
+    ];
+    console.log(books.length);
+    for (var i=0;i<books.length;i++) {
+        console.log(books[i].title + " by " + books[i].author.lastName);
+    }
 
     /**
      * TODO:
@@ -79,16 +299,12 @@
      *      ---
      *      ...
      */
-
-    books.forEach(funciton(books,i)) {
-        console.log("Book # " +(i+1))
-        console.log("Title: "+books.title)
-        console.log("Author: "+book.author.firstName + " " + book.author.lastName)
+    books.forEach(function(book, i) {
+        console.log("Book # "+(i+1))
+        console.log("Title: "+book.title)
+        console.log("Author: "+ book.author.firstName + " " + book.author.lastName)
         console.log("---")
-    }
-
-
-
+    });
 
     /**
      * Bonus:
@@ -101,25 +317,25 @@
      *   `showBookInfo` function.
      */
 
-    var createBook = function(title, authorFirstName, authorLastName) {
+    var title = "Hello";
+    var createBook = function(title, authorFirstName, authorLastName){
         var book = {};
         book.title = title;
-        book.author = {}
+        book.author = {};
         book.author.firstName = authorFirstName;
         book.author.lastName = authorLastName;
         return book;
-
     }
 
-    books.push(createBook(title: "Leviathan", authorFirstName:"Scott", authorLastName: "Westfield"))
+    books.push(createBook("Leviathan", "Scott", "Westfield"));
 
     var showBookInfo = function(book, i) {
-        console.log("Book # " +(i+1))
-        console.log("Title: "+books.title)
-        console.log("Author: "+book.author.firstName + " " + book.author.lastName)
+        console.log("Book # "+(i+1))
+        console.log("Title: "+book.title)
+        console.log("Author: "+ book.author.firstName + " " + book.author.lastName)
         console.log("---")
+
     }
     books.forEach(showBookInfo);
 
 })();
-
